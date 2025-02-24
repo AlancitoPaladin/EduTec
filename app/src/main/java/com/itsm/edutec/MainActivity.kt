da1b120d391e60798862876e074d1245149ff20b
AlancitoPaladin <alancarlos032104@gmail.com>
2025-02-24 12:18:12 -0600

    Second version

--- app/src/main/java/com/itsm/edutec/MainActivity.kt
+++ app/src/main/java/com/itsm/edutec/MainActivity.kt
@@ -4,13 +4,37 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.AccountCircle
+import androidx.compose.material.icons.rounded.Lock
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.itsm.edutec.ui.theme.EduTecTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +44,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             EduTecTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    LoginScreen(paddingValues = innerPadding)
+
                 }
             }
         }
@@ -31,17 +53,83 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun LoginScreen(paddingValues: PaddingValues) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var passwordVivible by remember { mutableStateOf(false) }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EduTecTheme {
-        Greeting("Android")
+    var emaiError by remember { mutableStateOf("") }
+    var passwordError by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Login",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.ExtraBold
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        TextField(
+            value = email,
+            onValueChange = { email = it },
+            label = {
+                Text(
+                    emaiError.ifEmpty { "Email" },
+                    color = if (emaiError.isNotEmpty()) Color.Red else Color.Black
+                )
+            },
+            leadingIcon = {
+                Icon(
+                    Icons.Rounded.AccountCircle,
+                    contentDescription = ""
+                )
+            },
+            shape = RoundedCornerShape(8.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 4.dp, horizontal = 20.dp),
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = Color.Transparent,
+                unfocusedLabelColor = Color.Transparent
+            )
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        TextField(
+            value = password,
+            onValueChange = { password = it }
+                    label = {
+                Text(
+                    passwordError.ifEmpty { "Password" },
+                    color = if (passwordError.isNotEmpty()) Color.Red else Color.Black
+                )
+            },
+            leadingIcon = {
+                Icon(
+                    Icons.Rounded.Lock,
+                    contentDescription = ""
+                )
+            },
+            visualTransformation = if (passwordVivible) VisualTransformation.None else PasswordVisualTransformation,
+            trailingIcon = {
+                val image = if (passwordVivible)
+                    painterResource(id = R.drawable.visibility_24px)
+                else painterResource(id = R.drawable.visibilitiy_off_24px)
+
+                Icon(
+                    painter = image,
+                    contentDescription = "",
+                    modifier = Modifier.clickable { passwordVivible = !passwordVivible }
+
+                )
+            }
+        )
     }
 }
