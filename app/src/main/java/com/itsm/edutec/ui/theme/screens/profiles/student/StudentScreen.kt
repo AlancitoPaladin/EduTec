73e2d61d624ae2831e119c57e88298c7b812e74d
AlancitoPaladin <alancarlos032104@gmail.com>
2025-03-06 12:36:49 -0600

    New changes of today

--- app/src/main/java/com/itsm/edutec/ui/theme/screens/profiles/student/StudentScreen.kt
+++ app/src/main/java/com/itsm/edutec/ui/theme/screens/profiles/student/StudentScreen.kt
@@ -1,5 +1,6 @@
 package com.itsm.edutec.ui.theme.screens.profiles.student
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -7,24 +8,37 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Apps
-import androidx.compose.material.icons.filled.Person4
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Star
-import androidx.compose.material3.BottomAppBar
+import androidx.compose.material.icons.outlined.Apps
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.Star
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -37,6 +51,7 @@ fun MyPreview() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun StudentScreen() {
+    val isDarkTheme = isSystemInDarkTheme()
     Scaffold(
         topBar = {
             TopAppBar(
@@ -45,27 +60,42 @@ fun StudentScreen() {
                     titleContentColor = MaterialTheme.colorScheme.onPrimary,
                 ),
                 title = {
-                    Text("EduTec")
-                }
+                    Text(
+                        text = "EduTec",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
+                navigationIcon = {
+                    IconButton(
+                        onClick = { /* do something */ },
+                        colors = IconButtonDefaults.iconButtonColors(
+                            contentColor = if (isDarkTheme) Color.White else Color.Cyan
+                        )
+                    ) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Localized description",
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(
+                        onClick = { /* do something */ },
+                        colors = IconButtonDefaults.iconButtonColors(
+                            contentColor = if (isDarkTheme) Color.White else Color.Cyan
+                        )
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.Menu,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
             )
         },
         bottomBar = {
-            BottomAppBar(
-                containerColor = MaterialTheme.colorScheme.onSurfaceVariant,
-                contentColor = MaterialTheme.colorScheme.onPrimary,
-            ) {
-                IconButton(onClick = {}) {
-                    Icon(Icons.Filled.Apps, contentDescription = "Localized description")
-                }
-
-                IconButton(onClick = {}) {
-                    Icon(Icons.Filled.Star, contentDescription = "Localized description")
-                }
-
-                IconButton(onClick = {}) {
-                    Icon(Icons.Filled.Person4, contentDescription = "Localized description")
-                }
-            }
+            BottomBar()
         }
     ) { innerPadding ->
         Column(
@@ -81,6 +111,7 @@ fun StudentScreen() {
             ) {
                 CoursesCard()
             }
+            // Column
         }
     }
 }
@@ -100,5 +131,30 @@ fun CoursesCard() {
                 .padding(16.dp),
             textAlign = TextAlign.Center,
         )
+    }
+}
+
+@Composable
+fun BottomBar() {
+    var selectedItem by remember { mutableIntStateOf(0) }
+    val items = listOf("Principal", "Novedades", "Favoritos")
+    val selectedIcons = listOf(Icons.Filled.Person, Icons.Filled.Apps, Icons.Filled.Star)
+    val unselectedIcons =
+        listOf(Icons.Outlined.Person, Icons.Outlined.Apps, Icons.Outlined.Star)
+
+    NavigationBar {
+        items.forEachIndexed { index, item ->
+            NavigationBarItem(
+                icon = {
+                    Icon(
+                        if (selectedItem == index) selectedIcons[index] else unselectedIcons[index],
+                        contentDescription = item
+                    )
+                },
+                label = { Text(item) },
+                selected = selectedItem == index,
+                onClick = { selectedItem = index }
+            )
+        }
     }
 }
